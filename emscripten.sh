831d3247b2de9e5a0528d639a7dce40d1b6c1791
Damien Bendejacq <jackbenfu@gmail.com>
2019-09-28 22:16:07 +0200

    Fix compilation flags for debug, release profile and release.

--- emscripten.sh
+++ emscripten.sh
@@ -7,9 +7,22 @@ YELLOW='\033[0;33m'
 
 pwd=$(pwd)
 
+echo ${pwd}
+
 debug=0
 release_profile=0
 release=0
+
+build() {
+  if [[ ${debug} == 1 ]]; then
+    make -f Makefile.emscripten debug
+  elif [[ ${release_profile} == 1 ]]; then
+    make -f Makefile.emscripten release_profile
+  elif [[ ${release} == 1 ]]; then
+    make -f Makefile.emscripten release
+  fi
+}
+
 if [[ $1 == "debug" ]]; then
   debug=1
   echo -e "${RED}/!\ DEBUG MODE ENABLED${NC}"
@@ -24,24 +37,16 @@ else
   exit 42
 fi
 
-echo -e "${GREEN}Building engine...${NC}"
+echo -e "${GREEN}Building ImGui...${NC}"
+cd ${pwd}/jackbengine/external/imgui
+make -f Makefile.emscripten
+
+echo -e "${GREEN}Building Jackbengine...${NC}"
 cd ${pwd}/jackbengine
-if [[ ${debug} == 1 ]]; then
-  make -f Makefile.emscripten debug
-elif [[ ${release_profile} == 1 ]]; then
-  make -f Makefile.emscripten release_profile
-elif [[ ${release} == 1 ]]; then
-  make -f Makefile.emscripten release
-fi
+build
 
 echo -e "${GREEN}Building application...${NC}"
 cd ${pwd}
-if [[ ${debug} == 1 ]]; then
-  make -f Makefile.emscripten debug
-elif [[ ${release_profile} == 1 ]]; then
-  make -f Makefile.emscripten release_profile
-elif [[ ${release} == 1 ]]; then
-  make -f Makefile.emscripten release
-fi
+build
 
 echo -ne '\007'
